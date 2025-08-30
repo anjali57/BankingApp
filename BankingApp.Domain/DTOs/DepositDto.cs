1e36c876376f258a33792c2eddeb3afaa0da9dac
Anjali Bhargava <anjalibhargava547@gmail.com>
2025-08-30 19:19:18 +0530

    Initial project commit

---
+++ BankingApp.Domain/DTOs/DepositDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.Domain.DTOs
+{
+    public class DepositDto
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ BankingApp.Domain/Interfaces/IAccountService.cs
@@ -0,0 +1,19 @@
+﻿using BankingApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.Domain.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<Account> CreateAccountAsync(Account account);
+        Task<Account?> GetAccountByNumberAsync(int accountNumber);
+        Task<IEnumerable<Account>> GetAllAccountsAsync();
+        Task<bool> DepositAsync(int accountNumber, decimal amount);
+        Task<bool> WithdrawAsync(int accountNumber, decimal amount);
+    }
+
+}
