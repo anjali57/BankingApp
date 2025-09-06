fe007eeb81221e71616bc510175e79c5038af779
Anjali Bhargava <anjalibhargava547@gmail.com>
2025-09-06 17:21:37 +0530

    assmbly info change

--- BankingApp.Domain/obj/Debug/net8.0/BankingApp.Domain.AssemblyInfo.cs
+++ BankingApp.Domain/obj/Debug/net8.0/BankingApp.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingApp.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d13404d73208e865310dd3f728d0a658c8d1ea99")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingApp.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingApp.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
